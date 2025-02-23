fbb11eed61a8919f7e7b36fe1bc50e8a9859bd28
Sophieeezsl <szhang11@smith.edu>
2025-02-22 21:12:52 -0500

    A3,sophie

--- Conversation.java
+++ Conversation.java
@@ -1,40 +1,103 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+//import come classes used later
 
-class Conversation implements Chatbot {
+class Conversation implements Chatbot{ 
 
-  // Attributes 
+// Attributes: there needs a transcript and some canned responses
+private ArrayList<String> transcript;
+private static String[] CannedResponses = {
+    "Interesting, tell me more.", 
+    "Mmm-hm.",
+    "Can you elaborate?",
+    "How does that make you feel?"
+};
 
-  /**
-   * Constructor 
-   */
-  Conversation() {
-    
-  }
+// Constructor
+  public Conversation() {
+    this.transcript = new ArrayList<>();
+}
 
   /**
    * Starts and runs the conversation with the user
    */
+
   public void chat() {
+//Conduct the chatting process, get round numbers and print out chatbox's lines
+    Scanner scanner = new Scanner(System.in);
 
-  }
+    System.out.println("How many rounds?");
+    int roundNum = scanner.nextInt();
+    scanner.nextLine();  // Consume the leftover newline character
+    System.out.println(roundNum);
 
-  /**
-   * Prints transcript of conversation
-   */
-  public void printTranscript() {
+    int counter = 0;
+            System.out.println("Hello! What's on your mind?");
 
-  }
+    while (counter < roundNum) {
+
+        
+        String input = scanner.nextLine(); 
+        String response = respond(input);
+
+        transcript.add(input);
+        transcript.add(response);
+
+        System.out.println(response);
+        counter++; 
+    }
+
+    System.out.println("Goodbye! The conversation is over.");
+    printTranscript();
+    scanner.close();
+}
+
+    /**
+     * Prints transcript of conversation
+     */
+    public void printTranscript() { //print out a transcript 
+      System.out.println("\n--- TRANSCRIPT ---");
+        for (String line : transcript) {
+            System.out.println(line);
+        }
+    }
 
   /**
    * Gives appropriate response (mirrored or canned) to user input
    * @param inputString the users last line of input
    * @return mirrored or canned response to user input  
    */
-  public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+
+  public String respond(String inputString) { // respond using canned responses, and change me to you
+// Mirror words mapping
+    ArrayList<String> originalWords = new ArrayList<>(Arrays.asList("I", "me", "am", "you", "my", "your"));
+    ArrayList<String> mirrorWords = new ArrayList<>(Arrays.asList("you", "you", "are", "I", "your", "my"));
+
+    // Split input string into words
+    String[] words = inputString.split(" ");
+    boolean mirrored = false;
+
+    // Check and replace mirror words
+    for (int i = 0; i < words.length; i++) {
+        int index = originalWords.indexOf(words[i]); // Find index of word in originalWords
+        if (index != -1) {  // If found, replace it
+            words[i] = mirrorWords.get(index);
+            mirrored = true;
+        }
+    }
+    // Join words back into sentence
+    String response = String.join(" ", words);
+      
+    if (mirrored) {
+      return response;
+    } else {
+      return CannedResponses[(int) (Math.random() * CannedResponses.length)];
+    }
   }
 
-  public static void main(String[] arguments) {
+
+  public static void main(String[] arguments) {//main argument
 
     Conversation myConversation = new Conversation();
     myConversation.chat();
--- reflection.md
+++ reflection.md
@@ -1,7 +1,18 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
-- Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
-- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+- Which classmates did you consult with while working on this assignment? Gemma
+- Which session(s) of TA / office hours did you attend? Tuesday 7-9
+- What are your initial impressions of Java? The "vocabs" for the language is infamiliar to me, and a little harder to get than python. But the structure of processing OOP progams is very understandable.
+- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? I do think Python is very good for procedural programming, and OOP can be done as well. I can see that Java is designed to be object oriented.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+The first thing I did was to review Java the language, as I did not fully catch up in class. (just a small suggestion: is it possible for us to go through coding examples in class just a little bit slower and clearer? Because I am so infamilliar with the language although I understand what the programming goal is, it takes me so much time to process and match specific coding pieces after class)
+
+I tried to plan out what function of the chatbox goes where and fill the plan in with specific codes. I do understand what I want the computer to perform but due to unfamiliarity with Java the language I can only ask TA for help very often because I just do not see what syntax I should use where.
+
+I hope there can be more tutorial on Java's syntax. Or just a friendly hint about the syntax we should be aware of can be very helpful. I will also preview the textbook often to self-study Java.
+
+2/22
+I encountered problems with running Java because my VS code had some trouble showing the run button. I spent days to figure it out. Finally I can run it and change my code based on the feedback.
+
